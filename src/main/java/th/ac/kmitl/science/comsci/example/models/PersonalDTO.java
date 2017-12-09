5cef8268c07ffc7bc8896f590fa0e53e5ed5d427
Mayrika Ⓟ <57050309@kmitl.ac.th>
2017-12-09 16:46:59 +0700

    feature: Create PersonalDTO.java and TestPersonalDTO.java

---
+++ src/main/java/th/ac/kmitl/science/comsci/example/models/PersonalDTO.java
@@ -0,0 +1,51 @@
+package th.ac.kmitl.science.comsci.example.models;
+
+public class PersonalDTO {
+
+    private String id;
+    private String globalId;
+    private String name;
+    private String taxId;
+    private String uriId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getGlobalId() {
+        return globalId;
+    }
+
+    public void setGlobalId(String globalId) {
+        this.globalId = globalId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTaxId() {
+        return taxId;
+    }
+
+    public void setTaxId(String taxId) {
+        this.taxId = taxId;
+    }
+
+    public String getUriId() {
+        return uriId;
+    }
+
+    public void setUriId(String uriId) {
+        this.uriId = uriId;
+    }
+
+}
